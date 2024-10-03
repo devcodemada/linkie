adcdfd428a705092a21f41e405b1354c005578ac
Devcodemada <devcodemada@gmail.com>
2024-10-03 10:56:15 +0700

    revs

--- services/imageService.js
+++ services/imageService.js
@@ -6,7 +6,7 @@ export const getUserImageSrc = (imagePath) => {
     if (imagePath) {
         return getSupabaseFileUrl(imagePath);
     } else {
-        return require("../assets/images/defaultUser.png");
+        return require("../assets/images/defaultuser.jpg");
     }
 };
 
